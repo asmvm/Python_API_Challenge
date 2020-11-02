29faedf3f2a13c82e28e7f5206b868f092287f53
ASMVM <66266053+asmvm@users.noreply.github.com>
2020-11-02 08:29:20 -0500

    weather py completed and pushed to repository

---
+++ Vacation_Py/.ipynb_checkpoints/api_keys-checkpoint.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "7989af206a6493a179daecd288b838a5"
+
+# Google API Key
+g_key = "AIzaSyDOyYei8577cmqTIiC8lawPDD2xtrwIIkQ"
---
+++ starter_code/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "22bbe0f7"
+
+# Google API Key
+g_key = "AIzaSyDOyYei8577cmqTIiC8lawPDD2xtrwIIkQ"
